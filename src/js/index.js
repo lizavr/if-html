bf9d5e3dbf7dfd066944ac77f114b3304c41a5bf
Lizaveta Rabetskaya <elizavetarabetskaya@gmail.com>
2022-11-27 12:44:31 +0300

    added swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
 const programName = 'Hello, World!';
 
 console.log(programName);
+import Swiper from 'swiper/bundle';
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  direction: getDirection(),
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  on: {
+    resize: function () {
+      swiper.changeDirection(getDirection());
+    },
+  },
+});
+
+function getDirection() {
+  var windowWidth = window.innerWidth;
+  var direction = window.innerWidth <= 760 ? 'vertical' : 'horizontal';
+
+  return direction;
+}
